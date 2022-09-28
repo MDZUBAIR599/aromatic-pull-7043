0f668ce91b8fdb54632727c12d22a22919d3575e
ramkrishnap43 <ramkrishnap.prabhu43@gmail.com>
2022-09-28 20:31:53 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Schedule from './Components/Schedule';
 ;
 
 function App() {
   return (
     <div className="App">
+      <Schedule />
     
     </div>
   );
