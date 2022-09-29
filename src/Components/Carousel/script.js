9b086a895cfad686fc2dbb5baf1c1c602b3850dd
Md Zubair <mdzubair5999@gmail.com>
2022-09-29 13:18:17 +0530

    slider_updated

--- src/Components/Carousel/script.js
+++ src/Components/Carousel/script.js
@@ -1,15 +1,15 @@
 import left_image from "../../Assets/slider/left_image.png";
 import middle_image from "../../Assets/slider/middle_image.png";
 import right_image from "../../Assets/slider/right_image.png";
-import right_arrow from "../../Assets/slider/right_arrow.png";
+import match_image from "../../Assets/slider/match_image.png";
 import left_arrow from "../../Assets/slider/left_arrow.png";
 import table_image from "../../Assets/slider/table_image.png";
 
 
 export const next=()=>{
     let i =1
-    const data = [left_image,right_image,table_image, middle_image]
-    const arr =[left_image,right_image,table_image, middle_image]
+    const data = [match_image,match_image,match_image, match_image]
+    const arr =[match_image,match_image, match_image]
     if (i > 4) {
       i = 0;
     }
@@ -24,8 +24,8 @@ export const next=()=>{
   export const  changelft=()=>{
     // const lft = [left_image,right_image,table_image, 
     let i =1
-    const data = [left_image,right_image,table_image, middle_image]
-    const arr =[left_image,right_image,table_image, middle_image,left_image,left_image,left_image,left_image]
+    const data = [match_image,right_image,match_image, match_image.png,match_image,match_image,match_image,match_image]
+    const arr =[match_image,right_image,match_image, match_image.png,match_image,match_image,match_image,match_image]
     if (i > 4) {
       i = 0;
     }
