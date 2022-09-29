673a4a2a0fd8cb8a20e8b0ecf843c9e3ea7d8d65
Robingeorge12 <robingsa.btch@gmail.com>
2022-09-29 21:19:24 +0530

    day-3 All cards updated

---
+++ src/Pages/Criclytics/CricHead.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Stack, SimpleGrid, Box, Text, VStack, Image, HStack, Button } from "@chakra-ui/react"
+export default function CricHead() {
+  return (
+  <Stack>
+<SimpleGrid colums={[]}>
+
+
+</SimpleGrid>
+
+
+  </Stack>
+  )
+}
