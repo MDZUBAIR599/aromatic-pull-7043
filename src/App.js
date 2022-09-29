4aa5aa903e8d0474e43e65ba076e0a8619f1e61b
Robingeorge12 <robingsa.btch@gmail.com>
2022-09-29 10:09:07 +0530

    day-2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Fantacy from './Pages/Fantacy/Fantacy';
 import FantacyHeader from './Pages/Fantacy/FantacyHeader';
-;
+
 
 
 
@@ -13,13 +13,12 @@ function App() {
   return (
     <div className="App">
 
-<<<<<<< HEAD
+
         {/* <Navbar/> */}
         <FantacyHeader />
         <Fantacy />
         
-=======
->>>>>>> 273a8116f6ba59208b10702a1390180057ccfce0
+
     </div>
   );
 }
