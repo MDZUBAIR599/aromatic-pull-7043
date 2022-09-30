070cbd4a99febbb69aececfd1d52c7c209ee12f3
Robingeorge12 <robingsa.btch@gmail.com>
2022-09-30 22:00:23 +0530

    day-4 work

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Criclytics from '../Pages/Criclytics/Criclytics'
 import Fantacy from '../Pages/Fantacy/Fantacy'
+import SecondPage from '../Pages/Fantacy/SecondPage'
 import HomePage from '../Pages/HomePage'
 
 function AllRoutes() {
@@ -11,6 +12,7 @@ function AllRoutes() {
           <Route path="/" element={<HomePage/>} />
           <Route path="/FantasyResearch" element={<Fantacy />} />
           <Route path="/Criclytics" element={<Criclytics />} /> 
+          <Route path={"/secondPage"} element={<SecondPage />} />
         </Routes>
     </div>
   )
