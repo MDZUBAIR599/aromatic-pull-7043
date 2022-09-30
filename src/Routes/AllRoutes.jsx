a7b1dcc911294c3b570981b3bce3b20fa132b754
Shashank Sinha <62157082+Shashanksinha20@users.noreply.github.com>
2022-09-30 11:15:09 +0530

    nc

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import SeriesPage from '../Pages/Series/SeriesPage'
-import HomePage from '../Pages/HomePage';
-
+import Criclytics from '../Pages/Criclytics/Criclytics'
+import Fantacy from '../Pages/Fantacy/Fantacy'
+import HomePage from '../Pages/HomePage'
+import SeriesPage from '../Pages/SeriesPage'
 
 function AllRoutes() {
   return (
     <div>
         <Routes>
           <Route path="/" element={<HomePage/>} />
-          <Route path = "/series" element = {<SeriesPage/>}/>
+          <Route path="/FantasyResearch" element={<Fantacy />} />
+          <Route path="/Criclytics" element={<Criclytics />} /> 
+          <Route path = "/series" element = {<SeriesPage />}/>
         </Routes>
     </div>
   )
