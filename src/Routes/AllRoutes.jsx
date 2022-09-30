dce2aa79764cda3f9a7b8851e768aa0739e0cd37
Md Zubair <mdzubair5999@gmail.com>
2022-09-30 23:24:53 +0530

    done

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -3,14 +3,13 @@ import { Route, Routes } from 'react-router-dom'
 import Navbar from '../Components/Navbar/Navbar'
 import Criclytics from '../Pages/Criclytics/Criclytics'
 import Fantacy from '../Pages/Fantacy/Fantacy'
-import SecondPage from '../Pages/Fantacy/SecondPage'
 import HomePage from '../Pages/HomePage'
 import PlayerPage from '../Pages/PlayerPage'
 import Singleplayer from '../Pages/playerPage/singleplayer/Singleplayer'
 import TeamPage from '../Pages/TeamPage'
 import Videos from '../Pages/videos-page/Videos'
 
-function AllRoutes(){
+function AllRoutes() {
   return (
     <>
    
@@ -22,8 +21,11 @@ function AllRoutes(){
           <Route path="/" element={<HomePage/>} />
           <Route path="/FantasyResearch" element={<Fantacy />} />
           <Route path="/Criclytics" element={<Criclytics />} /> 
-          <Route path="/videos" element={<Videos />} /> 
-
+          <Route path="/players" element={  <PlayerPage/>} /> 
+          <Route path="/teams" element={  <TeamPage/>} /> 
+          <Route path="/players/:id" element={  <Singleplayer/>} /> 
+          <Route path="/videos" element={  <Videos/>} /> 
+          
         </Routes>
     </div>
     </>
